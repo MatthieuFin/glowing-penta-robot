4dba5a1278989c2483eefd68a775835ee1c56641
Benjamin ZIGH <benjamin.zigh@gmail.com>
2014-02-11 17:48:23 +0100

    Eval.ml
    eval1 et eval

--- eval.ml
+++ eval.ml
@@ -1,4 +1,27 @@
 (* TODO *)
-let examine param =
-    ()
+let rec eval1 t = 
+    match t with
+      | True -> t
+      | False -> t
+      | Zero -> t
+      | Cond True t1 t2 -> t1
+      | Cond False t1 t2 -> t2
+      | Cond t1 t2 t3 -> Cond (eval1 t1) t2 t3
+      | Succ t1 -> Succ (eval1 t1)
+      | Pred (Succ v) when (v != True) && (v != False) -> v
+      | Pred Zero -> Zero
+      | Pred t1 -> Pred (eval1 t1)
+      | IsZero Zero -> True
+      | IsZero (Succ v) -> False (*ATTENTION: on ne vérifie pas que v est une valeur *)
+      | IsZero t1 -> IsZero (eval t1)
+      | _ -> t
 ;;
+
+
+let rec examine param =
+    match eval1 param as e with
+        | param -> param
+        | _ -> examine e
+;;
+
+let eval param = examine param;;
