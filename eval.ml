d19c75fc7e1e2dc0f2ac4a15bfd6aa847df96041
monSpeudo <worms.rockeur@gmail.com>
2014-02-11 17:54:22 +0100

    Corrections des erreurs de compilation

--- eval.ml
+++ eval.ml
@@ -1,27 +1,28 @@
+open Types;;
 (* TODO *)
 let rec eval1 t = 
     match t with
       | True -> t
       | False -> t
       | Zero -> t
-      | Cond True t1 t2 -> t1
-      | Cond False t1 t2 -> t2
-      | Cond t1 t2 t3 -> Cond (eval1 t1) t2 t3
+      | Cond (True, t1, t2) -> t1
+      | Cond (False, t1, t2) -> t2
+      | Cond (t1, t2, t3) -> Cond ((eval1 t1), t2, t3)
       | Succ t1 -> Succ (eval1 t1)
       | Pred (Succ v) when (v != True) && (v != False) -> v
       | Pred Zero -> Zero
       | Pred t1 -> Pred (eval1 t1)
       | IsZero Zero -> True
       | IsZero (Succ v) -> False (*ATTENTION: on ne vérifie pas que v est une valeur *)
-      | IsZero t1 -> IsZero (eval t1)
+      | IsZero t1 -> IsZero (eval1 t1)
       | _ -> t
 ;;
 
 
 let rec examine param =
-    match eval1 param as e with
+    match eval1 param with
         | param -> param
-        | _ -> examine e
+        | _ -> examine (eval1 param) 
 ;;
 
 let eval param = examine param;;
