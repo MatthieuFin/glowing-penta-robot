d5003c6c08f4039079657df03fcbe889f390d822
monSpeudo <worms.rockeur@gmail.com>
2014-02-12 17:06:36 +0100

    Première version de l'évaluation

--- TP03/eval.ml
+++ TP03/eval.ml
@@ -4,28 +4,12 @@
         | Benjamin ZIGH
 *)
 open Types;;
-let rec eval1 t = 
-    match t with
-      | True -> t
-      | False -> t
-      | Zero -> t
-      | Cond (True, t1, t2) -> t1
-      | Cond (False, t1, t2) -> t2
-      | Cond (t1, t2, t3) -> Cond ((eval1 t1), t2, t3)
-      | Succ t1 -> Succ (eval1 t1)
-      | Pred (Succ v) when (v != True) && (v != False) -> v
-      | Pred Zero -> Zero
-      | Pred t1 -> Pred (eval1 t1)
-      | IsZero Zero -> True
-      | IsZero (Succ v) -> False (*ATTENTION: on ne vérifie pas que v est une valeur *)
-      | IsZero t1 -> IsZero (eval1 t1)
-;;
+open Tools;;
 
-
-let rec examine param =
-    match (eval1 param) with
-        | evalres when evalres = param -> param
-        | _ -> examine (eval1 param) 
-;;
-
-let eval param = examine param;;
+let rec eval1 term =
+    match term with
+      | Var x -> getValue x
+      | Lambda (x, t) -> term
+      | App (Lambda(x, t), t2) -> (substitute x (eval1 t2) t)
+      | App (t1, t2) -> App ((eval1 t1), t2)
+;; 
