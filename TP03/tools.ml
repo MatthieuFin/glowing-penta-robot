55591094c4085df4710a291f9ca90175952a5121
monSpeudo <worms.rockeur@gmail.com>
2014-02-18 21:05:24 +0100

    Update tools.ml
    Déclaration fonctionnelle

--- TP03/tools.ml
+++ TP03/tools.ml
@@ -5,23 +5,25 @@
 *)
 open Types ;;
 
+exception UnboundValue;;
+
+let tblOfSymbols = Hashtbl.create 1;;
 
 (*TODO Affecte la valeur value a l'alias alias *)
-let declare alias value =   0;;
+let declare (alias : string) (value : term) = 
+    Hashtbl.replace tblOfSymbols alias value;
+    value
+;;
 
-
-
-(* C'est quoi ces trucs? *)
-
+(* Remplace les occurence d'une variable par sa valeur *)
 let rec substitute x y t =
   Var "x"
 ;;
 
-let rec getValue t =
-  Lambda ("x", Var "a")
+(* Donne la valeur d'une variable *)
+let getValue alias = 
+    try Hashtbl.find tblOfSymbols alias
+    with _ -> Var alias
 ;;
 
-let declare var valuel =
-  Var var
-;;
 
