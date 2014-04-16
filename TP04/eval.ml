745c2970e4046f07f24eafce4482b20f8fe314c2
monSpeudo <worms.rockeur@gmail.com>
2014-04-16 16:16:19 +0200

    Correction règle application.

--- TP04/eval.ml
+++ TP04/eval.ml
@@ -7,40 +7,53 @@ open Types;;
 open TypeChecker;;
 open Tools;;
 
+let rec is_n_val t =
+    match t with
+      | Zero -> true
+      | Succ t' -> is_n_val t'
+      | Pred t' -> is_n_val t'
+      | _ -> false
+;;
+
+
+let is_val t =
+ match t with
+   | True -> true
+   | False -> true
+   | Lambda _ -> true
+   | t' -> is_n_val t'
+;;
 
 (* TODO Tester eval1 *)
-let rec eval1 t =
+let rec examine t = 
+    let t' = eval1 t in
+    if t' = t then t else examine t'
+and eval1 t =
     match t with
-      | True -> t
-      | False -> t
-      | Zero -> t
-      | Cond (True, t1, t2) -> t1
-      | Cond (False, t1, t2) -> t2
-      | Cond (t1, t2, t3) -> Cond ((eval1 t1), t2, t3)
-      | Succ t1 -> Succ (eval1 t1)
-      | Pred (Succ t1)  -> t1
-      | Pred Zero -> Zero
-      | Pred t1 -> Pred (eval1 t1)
-      | IsZero Zero -> True
-      | IsZero (Succ v) -> False (*ATTENTION: on ne vérifie pas que v est une valeur *)
-      | IsZero t1 -> IsZero (eval1 t1)
-      | Var x -> begin
-                    match getValue x  with 
-                        | None -> failwith ("Variable inconnue " ^ x)
-                        | Some t' -> t'
-                 end
-      | Lambda (ty, x, t') -> t
-      | App (Lambda(ty, x, t), t2) -> (substitute ty x (eval1 t2) t)
+      | True ->  t
+      | False ->  t
+      | Zero ->  t
+      | Cond (True, t1, t2) ->  t1
+      | Cond (False, t1, t2) ->  t2
+      | Cond (t1, t2, t3) ->  Cond ((eval1 t1), t2, t3)
+      | Succ t1 ->  Succ (eval1 t1)
+      | Pred (Succ t1)  ->  t1
+      | Pred Zero ->  Zero
+      | Pred t1 ->  Pred (eval1 t1)
+      | IsZero Zero ->  True
+      | IsZero (Succ v) ->  False (*ATTENTION: on ne vérifie pas que v est une valeur *)
+      | IsZero t1 ->  IsZero (eval1 t1)
+      | Var x -> 
+            begin
+                match getValue x  with 
+                    | None -> failwith ("Variable inconnue " ^ x)
+                    | Some t' -> t'
+            end
+      | Lambda (ty, x, t') ->  t
+      | App (Lambda(ty, x, t'), v2) when (is_val v2) -> (substitute ty x v2 t')
+      | App (t1, t2) when (is_val t1) -> App(t1, (eval1 t2))
       | App (t1, t2) -> App ((eval1 t1), t2)
 ;; 
-
-
-
-let rec examine t = 
-    match eval1 t with
-        | t' when t' = t -> t
-        | _ -> examine (eval1 t)
-;;
 
 let eval param = 
 let type_p = typeof param [] in
--- TP04/tools.ml
+++ TP04/tools.ml
@@ -20,9 +20,9 @@ let rec substitute ty t1 t2 t =
       | Var x when x = t1 -> t2
       | App (t3, t4) -> App ((substitute ty t1 t2 t3), (substitute ty t1 t2 t4))
       | Lambda (ty, x, t3) when x <> t1 -> Lambda (ty, x, (substitute ty t1 t2 t3))
-      | Pred x -> Pred (substitute ty t1 t2 t)
-      | IsZero x -> IsZero (substitute ty t1 t2 t)
-      | Succ x -> Succ (substitute ty t1 t2 t)
+      | Pred x -> Pred (substitute ty t1 t2 x)
+      | IsZero x -> IsZero (substitute ty t1 t2 x)
+      | Succ x -> Succ (substitute ty t1 t2 x)
       | Cond (bo, tr, fa) -> Cond ((substitute ty t1 t2 bo), (substitute ty t1 t2 tr), (substitute ty t1 t2 fa))
       | _-> t
 ;;
