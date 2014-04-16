b0a0bb2647e48ccb5aa62df97d5e10df0a6f8868
monSpeudo <worms.rockeur@gmail.com>
2014-04-16 15:20:11 +0200

    Ajout d'espace dans la sortie.

--- TP04/output.ml
+++ TP04/output.ml
@@ -24,16 +24,16 @@ let rec term_to_string term =
       | True -> "True"
       | False -> "False"
       | Zero -> "0"
-      | Cond (c, t, f) -> "If (" ^ (term_to_string c) ^ ") Then"
+      | Cond (c, t, f) -> "If (" ^ (term_to_string c) ^ ") Then "
                           ^ (term_to_string t) 
-                          ^ "Else" 
+                          ^ "Else " 
                           ^ (term_to_string f)
       | Succ (n) -> "Succ " ^ (term_to_string n)
       | Pred (n) -> "Pred " ^ (term_to_string n)
       | IsZero (n) -> "IsZero " ^ (term_to_string n)
       | Var (s) -> s
       | App (t1, t2) -> (term_to_string t1) ^ " " ^ (term_to_string t2)
-      | Lambda (typ, var, t) -> "λ" ^ var ^ ":" ^ (type_to_string typ) ^ "." ^ (term_to_string t)
+      | Lambda (typ, var, t) -> "λ" ^ var ^ " : " ^ (type_to_string typ) ^ ". " ^ (term_to_string t)
 ;;
 
 
